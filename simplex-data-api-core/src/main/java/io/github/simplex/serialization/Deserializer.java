75ff2c7d9f9768e879332258d2ffd8a8f3a513d2
SD <harrypotter.starwars.shrish@gmail.com>
2020-09-22 08:48:06 +0530

    Modular api
     Changes to be committed:
    	modified:   build.gradle
    	modified:   gradle.properties
    	modified:   settings.gradle
    	new file:   simplex-data-api-core/build.gradle
    	renamed:    src/main/java/io/github/simplex/serialization/Main.java -> simplex-data-api-core/src/main/java/io/github/simplex/serialization/Deserializer.java
    	renamed:    src/main/java/io/github/simplex/serialization/object/Compostable.java -> simplex-data-api-core/src/main/java/io/github/simplex/serialization/object/Compostable.java
    	renamed:    src/main/java/io/github/simplex/serialization/object/Flammable.java -> simplex-data-api-core/src/main/java/io/github/simplex/serialization/object/Flammable.java
    	renamed:    src/main/java/io/github/simplex/serialization/object/Fuel.java -> simplex-data-api-core/src/main/java/io/github/simplex/serialization/object/Fuel.java
    	renamed:    src/main/java/io/github/simplex/serialization/object/Paveable.java -> simplex-data-api-core/src/main/java/io/github/simplex/serialization/object/Paveable.java
    	renamed:    src/main/java/io/github/simplex/serialization/util/IdentifiableCodecs.java -> simplex-data-api-core/src/main/java/io/github/simplex/serialization/util/IdentifiableCodecs.java
    	new file:   simplex-data-api-core/src/main/resources/assets/simplex-data-api-core/icon.png
    	new file:   simplex-data-api-core/src/main/resources/fabric.mod.json
    	renamed:    src/main/resources/simplex-data-api.mixins.json -> simplex-data-api-core/src/main/resources/simplex-data-api-core.mixins.json
    	deleted:    src/main/java/io/github/simplex/serialization/object/Entries.java
    	modified:   src/main/resources/fabric.mod.json

--- simplex-data-api-core/src/main/java/io/github/simplex/serialization/Deserializer.java
+++ simplex-data-api-core/src/main/java/io/github/simplex/serialization/Deserializer.java
@@ -5,11 +5,11 @@ import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class Main implements ModInitializer {
+public class Deserializer implements ModInitializer {
 
     public static Logger LOGGER = LogManager.getLogger();
 
-    public static final String MOD_ID = "simplex-data-api";
+    public static final String MOD_ID = "assets/simplex-data-api";
     public static final String MOD_NAME = "Simplex Data API";
 
     @Override
